c6b992fba2a176075035560061c8b7e528808e6d
Bruce191 <mainumarsaad@gmail.com>
2026-07-06 11:25:49 +0100

    added psycog 2 to reqs.txt

--- backend/api_backend/Database/db.py
+++ backend/api_backend/Database/db.py
@@ -28,10 +28,6 @@ from pathlib import Path
 #         db.close()
 
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from pathlib import Path
-
 db_path = Path(__file__).parent / "local.db"
 
 DATABASE_URL = os.environ.get("DATABASE_URL", f"sqlite:///{db_path.absolute()}")
